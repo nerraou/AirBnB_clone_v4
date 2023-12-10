be0cf22ec0ecdc6d3c7f4bf388c2971fbebcfef2
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-12-10 14:06:38 +0100

    feat: select menities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,25 @@
+let amentities = [];
 
-const amentitiesId = []
+$('[data-id] input').change((e) => {
+  const self = $(e.target);
+  const parent = self.parent();
 
-$("[data-id] input").change((e) => {
-	if ($(e.target).is(":checked"))
-			
- });
+  const dataId = parent.data('id');
+  const dataName = parent.data('name');
+
+  if (self.is(':checked')) {
+    amentities.push({ id: dataId, name: dataName });
+  } else {
+    amentities = amentities.filter((amentiy) => {
+      return amentiy.id !== dataId;
+    });
+  }
+
+  const amentitiesString = amentities
+    .map((amentiy) => {
+      return amentiy.name;
+    })
+    .join(', ');
+
+  $('#amenities_list').text(amentitiesString);
+});
